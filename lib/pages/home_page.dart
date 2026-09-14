431bc9b5adde486c515d39b70adebefad675cf86
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-09-14 22:50:03 +0600

    update: home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: Column(children: []),
+      body: Column(children: [Center(child: Text('Home Page'))]),
     );
   }
 }
