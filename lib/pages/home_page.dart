4d3a34c267a9c2c8fdb79f977575d3cb8a7e3107
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-09-18 23:34:45 +0600

    update: home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const new({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  fetchData() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+
+    print(response.body);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +32,7 @@ class HomePage extends StatelessWidget {
         children: [
           Center(child: Text('Home Page')),
           Text('Hello World'),
-          Text('API Integration')
+          Text('API Integration'),
         ],
       ),
     );
