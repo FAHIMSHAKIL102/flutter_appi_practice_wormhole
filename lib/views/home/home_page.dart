92d40c84cf7b4b8391887279ba24b1261ef891af
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-09-21 19:43:03 +0600

    update: future builder

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,9 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_appi_practice_wormhole/models/posts_model.dart';
+//import 'package:flutter_appi_practice_wormhole/models/posts_model.dart';
 import 'package:flutter_appi_practice_wormhole/services/api_services.dart';
-import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const new({super.key});
@@ -13,35 +10,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<PostsModel> posts = [];
+  // List<PostsModel> posts = [];
 
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchData();
+  // }
 
-  fetchData() async {
-    final data = await ApiServices.fetchData();
+  // fetchData() async {
+  //   final data = await ApiServices.fetchData();
 
-    print(data!.length);
-    setState(() {
-      posts = data;
-    });
-  }
+  //   print(data!.length);
+  //   setState(() {
+  //     posts = data;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: Column(
-        mainAxisAlignment: .center,
-        crossAxisAlignment: .center,
-        children: [
-          Expanded(
-            child: ListView.builder(
+      body: FutureBuilder(
+        future: ApiServices.fetchData(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error'));
+          } else {
+            return ListView.builder(
               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              itemCount: posts.length,
+              itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
@@ -55,11 +55,11 @@ class _HomePageState extends State<HomePage> {
                           Icon(Icons.broken_image_outlined),
                     ),
                     title: Text(
-                      posts[index].title,
+                      snapshot.data![index].title,
                       style: TextStyle(fontSize: 20),
                     ),
                     subtitle: Text(
-                      posts[index].body,
+                      snapshot.data![index].body,
                       style: TextStyle(fontSize: 15),
                       maxLines: 2,
                       overflow: TextOverflow.ellipsis,
@@ -67,9 +67,9 @@ class _HomePageState extends State<HomePage> {
                   ),
                 );
               },
-            ),
-          ),
-        ],
+            );
+          }
+        },
       ),
     );
   }
